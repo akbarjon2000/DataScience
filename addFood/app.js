ec88d38b976d80b18cebe305e5bd1186ee286c61
akbarjon2000 <1543431a@gmail.com>
2021-11-23 01:05:38 +0900

    add food project

--- addFood/app.js
+++ addFood/app.js
@@ -1,16 +1,50 @@
-const inputs = document.querySelectorAll("input")
-console.log(inputs)
+
 var list = document.getElementById("list")
 var par = document.createElement('p')
 var img = document.createElement('img');
 // var form = document.forms["my-form"]
 const handleSubmit = (event) => {
     event.preventDefault();
- 
-    console.log(list)
-    const { foodName, price, description, category, url } = document.forms["my-form"]
-console.log()
-const newEl = "hello";
-    list.innerHTML += newEl;
-    event.target.reset()
+
+    const foodName = document.getElementById("foodName").value;
+    const price = document.getElementById("price").value;
+    const description = document.getElementById("description").value;
+    const category = document.getElementById("category").value;
+    const url = document.getElementById("url").value;
+    let menu = [
+        {
+            id: Date.now(),
+            foodName,
+            price,
+            description,
+            category,
+            url,
+        }
+    ]
+    var MyFood = [];
+    MyFood.push(menu);
+    console.log(MyFood)
+    localStorage.setItem('MyFood', MyFood)
+    console.log(localStorage)
+    list.innerHTML = `<div style = "background-image: url(${url});   width: 400px;
+  height: 300px;
+  background-size: cover;
+  background-position: center;
+  border-radius: 10px;
+  border: 5px solid orange;"></div>
+    <div style = "display:flex; flex-direction: column; align-items: center; width:500px; padding: 20px;">   
+    <p class = 'category'>${category}</p>
+    <div class = "firstLine">
+    <p class = 'foodName'>${foodName}</p>
+    <p class = 'price'>$ ${price}</p>
+    </div>
+    <p class = 'description'>${description}</p>
+    </div>
+    `
+
+    document.getElementById("foodName").value = ''
+    document.getElementById("category").value = ''
+    document.getElementById("price").value = ''
+    document.getElementById("description").value = ''
+    document.getElementById("url").value = ''
 }
--- addFood/dist/app.dev.js
+++ addFood/dist/app.dev.js
@@ -1,22 +1,33 @@
 "use strict";
 
-var inputs = document.querySelectorAll("input");
 var list = document.getElementById("list");
 var par = document.createElement('p');
 var img = document.createElement('img'); // var form = document.forms["my-form"]
 
 var handleSubmit = function handleSubmit(event) {
   event.preventDefault();
-  console.log(event.target[0].value);
-  console.log(list);
-  var _document$forms$myFo = document.forms["my-form"],
-      foodName = _document$forms$myFo.foodName,
-      price = _document$forms$myFo.price,
-      description = _document$forms$myFo.description,
-      category = _document$forms$myFo.category,
-      url = _document$forms$myFo.url;
-  event.target.value ? alert("good") : alert("fiil in the form");
-  var newEl = "\n        <li>\n\n            <h1>".concat(foodName.value, "</h1>\n        </li>\n        \n    ");
-  list.innerHTML += newEl;
-  event.target.reset();
+  var foodName = document.getElementById("foodName").value;
+  var price = document.getElementById("price").value;
+  var description = document.getElementById("description").value;
+  var category = document.getElementById("category").value;
+  var url = document.getElementById("url").value;
+  var menu = [{
+    id: Date.now(),
+    foodName: foodName,
+    price: price,
+    description: description,
+    category: category,
+    url: url
+  }];
+  var MyFood = [];
+  MyFood.push(menu);
+  console.log(MyFood);
+  localStorage.setItem('MyFood', MyFood);
+  console.log(localStorage);
+  list.innerHTML = "<div style = \"background-image: url(".concat(url, ");   width: 400px;\n  height: 300px;\n  background-size: cover;\n  background-position: center;\n  border-radius: 10px;\n  border: 5px solid orange;\"></div>\n    <div style = \"display:flex; flex-direction: column; align-items: center; width:500px; padding: 20px;\">   \n    <p class = 'category'>").concat(category, "</p>\n    <div class = \"firstLine\">\n    <p class = 'foodName'>").concat(foodName, "</p>\n    <p class = 'price'>$ ").concat(price, "</p>\n    </div>\n    <p class = 'description'>").concat(description, "</p>\n    </div>\n    ");
+  document.getElementById("foodName").value = '';
+  document.getElementById("category").value = '';
+  document.getElementById("price").value = '';
+  document.getElementById("description").value = '';
+  document.getElementById("url").value = '';
 };
